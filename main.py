4122515ffda661ca96e3954d7901c90be82721d7
Rostyslaw <kivrostylav@gmail.com>
2025-09-19 11:11:32 +0000

    001

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from rich.prompt import Prompt
+from rich.console import Console
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    console = Console()
+    firstname = Prompt.ask("Enter firstname", console=console)
+    lastname = Prompt.ask("Enter lastname", console=console)
+    age = Prompt.ask("Enter age", console=console)
 
+    console.print("{firstname} {lastname}, {age} р.н."
+                  .format(firstname=firstname, lastname=lastname, age=age))
+    console.print("Прізвище: {lastname}\nІм'я: {firstname}\nРік народження: {age}"
+                  .format(lastname=lastname, firstname=firstname, age=age))
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    main()
